35c8c5b4280f3b00ce348d2587932540857829bf
Kyrill Langhans <kyrilllanghans@irisglobal.org>
2025-09-28 12:43:30 +0200

    feat: added i18n

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -13,7 +13,27 @@ import { getResources } from "./resources";
 import "moment/locale/pt";
 import { SelectMultipleProvider } from "@contexts/select-multiple";
 import { routerProvider } from "@providers/router-provider";
-import { useLocale } from "@i18n/i18n-provider";
+import { myI18n, useLocale } from "@i18n/i18n-provider";
+
+myI18n.addResourceBundle("en", "List", {
+  "actions.list": "List",
+});
+myI18n.addResourceBundle("en", "Create", {
+  "actions.create": "Create",
+});
+myI18n.addResourceBundle("en", "Delete", {
+  "actions.delete": "Delete",
+});
+
+myI18n.addResourceBundle("pt", "List", {
+  "actions.list": "Lista",
+});
+myI18n.addResourceBundle("pt", "Create", {
+  "actions.create": "Criar",
+});
+myI18n.addResourceBundle("pt", "Delete", {
+  "actions.delete": "Apagar",
+});
 
 export const LayoutClient: React.FC<
   { children: React.ReactNode }
@@ -35,6 +55,15 @@ export const LayoutClient: React.FC<
           dataProvider={dataProvider}
           notificationProvider={useNotificationProvider}
           resources={localizedResources}
+          i18nProvider={{
+            translate: (key: string, ns?: string) => {
+              return myI18n.t(key, { ns }).toString() || key;
+            },
+            changeLocale: (lng: string) =>
+              myI18n.changeLanguage(lng),
+            getLocale: () =>
+              myI18n.language,
+          }}
           options={{
             syncWithLocation: true,
             warnWhenUnsavedChanges: true,
